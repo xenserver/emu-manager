a606d5240c7272688c81d38e11cac9d0070d9735
Ross Lagerwall <ross.lagerwall@citrix.com>
2017-11-20 11:06:50 +0000

    Add a prototype for exported function

--- em-client.h
+++ em-client.h
@@ -30,6 +30,7 @@ extern struct command_names commands[];
 int em_socke_send_cmd(emu_socket_t* sock, enum command_num cmd_no);
 int em_socke_send_cmd_fd(emu_socket_t* sock, enum command_num cmd_no, int fd);
 int em_socke_send_cmd_args(emu_socket_t* sock, enum command_num cmd_no, struct args_list* args);
+int em_socke_send_cmd_fd_args(emu_socket_t* sock, enum command_num cmd_no, int fd, struct args_list* args);
 
 int em_socket_alloc(emu_socket_t **sock, em_socket_callback callback, void* data);
 int em_socket_open(emu_socket_t *sock, char* path);
